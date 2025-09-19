a3214b15bbadd83eb47b98b74fb36cd3dbfdbd6a
tchirag03 <thippeswamychirag@gmail.com>
2025-09-19 07:18:04 +0530

    refactor(database): simplify MongoDB connection and improve error handling

--- backend/database/dbConnect.js
+++ backend/database/dbConnect.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config({ path: "./backend/.env" });
+dotenv.config();
 
 export const dbConnect = async () => {
   try {
-    await mongoose.connect(await process.env.MONGO_URI).then(() => {
-    console.log(" Successfully Connected to DB");
-  });
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Successfully Connected to DB");
   } catch (error) {
-    console.log(error)
+    console.log("Database connection error:", error);
   }
 };
